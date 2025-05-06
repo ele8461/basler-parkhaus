ea329a09d3da99e334801809dc996a6cdfabcaa0
ele8461 <elena.fankhauser@stud.fhgr.ch>
2025-05-06 15:10:07 +0200

    fetch

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
 console.log('hoi')
+
+const url = 'https://data.bs.ch/api/explore/v2.1/catalog/datasets/100088/records?limit=20';
+async function loadTimetable() {
+    try {
+        const response = await fetch(url);
+        return await response.json();
+    } catch (error) {
+        console.error(error);
+        return false;
+    }
+}
+const timetable = await loadTimetable();
+console.log(timetable); // gibt die Daten der API oder false in der Konsole aus
