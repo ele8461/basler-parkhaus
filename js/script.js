82fbc283b3be4d642691f3a953f2c6872275eab3
ele8461 <elena.fankhauser@stud.fhgr.ch>
2025-05-26 15:37:51 +0200

    pop-up

--- js/script.js
+++ js/script.js
@@ -13,6 +13,61 @@ async function loadTimetable() {
 const timetable = await loadTimetable();
 console.log(timetable); // gibt die Daten der API oder false in der Konsole aus
 
+// Datenstruktur der Parkhäuser
+const parkHouses = [
+  {
+    id: "baselparkhauseurope",
+    name: "Parkhaus Europe",
+    freie_plaetze: 85,
+    auslastung: 65
+  },
+  {
+    id: "baselparkhausclarahuus",
+    name: "Parkhaus Clarahuus",
+    freie_plaetze: 120,
+    auslastung: 40
+  },
+  {
+    id: "baselparkhausrebgasse",
+    name: "Parkhaus Rebgasse",
+    freie_plaetze: 40,
+    auslastung: 85
+  },
+  {
+    id: "baselparkhausstorchen",
+    name: "Parkhaus Storchen",
+    freie_plaetze: 60,
+    auslastung: 70
+  }
+];
+
+
+// Alle Buttons mit der Klasse "parkhaus_flaeche" holen
+const buttons = document.querySelectorAll(".parkhaus_flaeche");
+
+// EventListener zu jedem Button hinzufügen
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+    const hausId = button.id;
+    const parkhaus = parkHouses.find(h => h.id === hausId);
+
+    if (parkhaus) {
+      showDialog(parkhaus);
+    } else {
+      console.warn(`Kein Parkhaus mit ID "${hausId}" gefunden.`);
+    }
+  });
+});
+
+// Funktion zur Anzeige der Parkhausdaten
+function showDialog(parkhaus) {
+  alert(
+    `${parkhaus.name}\n` +
+    `Freie Plätze: ${parkhaus.freie_plaetze}\n` +
+    `Auslastung: ${parkhaus.auslastung}%`
+  );
+}
+
 
 // was wir brauchen vom datensatz:
 //title
